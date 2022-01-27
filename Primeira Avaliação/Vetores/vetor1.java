89620e2f1237b7e65f29081b7e3dfbdee6a47d0c
Mateusfmelo <mateusmelo51@gmail.com>
2022-01-27 18:33:04 -0300

    Atualização

--- Primeira Avaliação/Vetores/vetor1.java
+++ Primeira Avaliação/Vetores/vetor1.java
@@ -2,26 +2,26 @@ import java.util.Scanner;
 
 public class vetor1 {
 
-    public static void main(String[] args) {
-      Scanner ler = new Scanner(System.in);
+  public static void main(String[] args) {
+    Scanner ler = new Scanner(System.in);
 
-      int n = 5; // tamanho dos vetores
-      int a[] = new int[n]; // declarando os vetores de nros inteiros "a" e "b" de tamanho "n"
-      int b[] = new int[n];
-      int i; // indice ou posicao
+    int n = 5;
+    int a[] = new int[n];
+    int b[] = new int[n];
+    int i;
 
-      for (i=0; i<n; i++) {
-        System.out.printf("Informe %do. elemento de %d: ", (i+1), n);
-        a[i] = ler.nextInt();
-      }
-
-      for (i=0; i<n; i++) {
-        b[i] = a[i];
-      }
-
-      System.out.printf("\n\n");
-      for (i=0; i<n; i++) {
-        System.out.printf("a[%d] = %3d   b[%d] = %3d\n", i, a[i], i, b[i]);
-      }
+    for (i = 0; i < n; i++) {
+      System.out.printf("Informe %do. elemento de %d: ", (i + 1), n);
+      a[i] = ler.nextInt();
     }
-}
+
+    for (i = 0; i < n; i++) {
+      b[i] = a[i];
+    }
+
+    System.out.printf("\n\n");
+    for (i = 0; i < n; i++) {
+      System.out.printf("a[%d] = %3d   b[%d] = %3d\n", i, a[i], i, b[i]);
+    }
+  }
+}      
---
+++ Primeira Avaliação/Vetores/vetor3.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class vetor3 {
+
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+
+        int n = 15;
+        int a[] = new int[n];
+        int b[] = new int[n];
+        int i;
+
+        for (i = 0; i < n; i++) {
+            System.out.printf("Informe o %2do. elemento de %d: ", (i + 1), n);
+            a[i] = ler.nextInt();
+        }
+
+        for (i = 0; i < n; i++) {
+            b[i] = a[i] * a[i];
+        }
+
+        System.out.printf("\n\n i- a[i]   b[i] = a[i] * a[i]\n");
+        System.out.printf("=============================\n");
+        for (i = 0; i < n; i++) {
+            System.out.printf("%2d- %4d   %4d\n", i, a[i], b[i]);
+        }
+        System.out.printf("=============================\n");
+    }
+}
